a2f760fc3d1357c75c3e20e87268fd72dc268e11
Suliman Hassan Algaramanli <salgaramanli1@gmail.com>
2023-05-24 15:54:11 +0200

    CS437 hw 1  (:

--- main.cpp
+++ main.cpp
@@ -16,14 +16,11 @@ struct Node
 int n ,Now = 0  ,index ,minimum;
 string order;
 Node SJF[Max];
-
-
 bool RemainingTimeIsZero()
 {
     for(int i=0 ;i<n; i++)
         if(SJF[i].rem != 0)
             return 1;
-
     return 0;
 }
 void sorting()
@@ -44,6 +41,8 @@ void sorting()
 }
 void SRTF()
 {
+    order="";
+    Now = 0;
     sorting();
     while( RemainingTimeIsZero() )
     {
@@ -71,11 +70,8 @@ void SRTF()
             SJF[index].rem --;
             Now ++;
             SJF[index].fin = Now;
-
         }
     }
-
-    cout<<index<<"  --  now : "<<Now<<endl<<endl;
     //for Turnaround Time
     for(int i=0;i<n;i++)
     {
@@ -96,7 +92,7 @@ void SRTF()
     {
         cout<<SJF[i].ch<<"\t"<<SJF[i].arv<<"\t"<<SJF[i].run<<"\t"<<SJF[i].rem<<"\t"<<SJF[i].fin<<"\t"<<SJF[i].tat<<"\t"<<SJF[i].wait<<endl;
     }
-    cout<<index<<"  --  now : "<<Now<<endl<<endl;
+    cout<<"Time now : "<<Now<<endl<<endl;
     cout<<"order  is : "<<order<<endl;
     cout<<"-----------------------------"<<endl;
 }
@@ -105,15 +101,17 @@ bool ExecutionNotEmpty()
     for(int i=0 ;i<n; i++)
         if(SJF[i].exe == -1)
             return 1;
-
     return 0;
 }
 void SJF_()
 {
+    order="";
+    Now = 0;
     sorting();
     while( ExecutionNotEmpty() )
     {
-        int minimum = 0;
+        minimum = 0;
+        index = 0;
         for(int i=0; i<=n ;i++)
         {
             if(SJF[i].exe == -1  && SJF[i].arv <= Now)
@@ -136,8 +134,6 @@ void SJF_()
             Now += SJF[index].run;
         }
     }
-
-
 //for waiting time
     float sum = 0;
     for(int i=0;i<n;i++)
@@ -146,19 +142,16 @@ void SJF_()
         cout<<"wait time for "<<SJF[i].ch<<" : "<<SJF[i].wait<<endl;
         sum += SJF[i].wait;
     }
-    cout<<"Avrege time is : "<<sum <<" / "<<n<<" = "<<sum/n<<endl;
-
-
+    cout<<"Avrege time is : "<<sum <<" / "<<n<<" = "<<sum/n<<endl<<endl;
 //print
     cout<<"\tArv\tRun\tExe\twait "<<endl;
     for(int i=0;i<n;i++)
     {
         cout<<SJF[i].ch<<"\t"<<SJF[i].arv<<"\t"<<SJF[i].run<<"\t"<<SJF[i].exe<<"\t"<<SJF[i].wait<<endl;
     }
-    cout<<index<<"  --  now : "<<Now<<endl<<endl;
-
-        cout<<"order  is : "<<order<<endl;
-
+    cout<<"Time now : "<<Now<<endl<<endl;
+    cout<<"order  is : "<<order<<endl;
+    cout<<"-----------------------------"<<endl;
 }
 int main()
 {
@@ -180,62 +173,70 @@ int main()
         SJF[i].tat = 0;
         cout<<"-----------------------------"<<endl;
     }
-
+/*
+    n=4;
+    //--------------------
+    SJF[0].ch = 'A';
+    SJF[0].arv = 0;
+    SJF[0].run = 9;
+    SJF[0].rem = SJF[0].run;
+    SJF[0].exe = -1;
+    SJF[0].fin = 0;
+    SJF[0].wait = 0;
+    SJF[0].tat = 0;
+    //--------------------
+    SJF[1].ch = 'B';
+    SJF[1].arv = 2;
+    SJF[1].run = 3;
+    SJF[1].rem = SJF[1].run;
+    SJF[1].exe = -1;
+    SJF[1].fin = 0;
+    SJF[1].wait = 0;
+    SJF[1].tat = 0;
+    //--------------------
+    SJF[2].ch = 'C';
+    SJF[2].arv = 3;
+    SJF[2].run = 1;
+    SJF[2].rem = SJF[2].run;
+    SJF[2].exe = -1;
+    SJF[2].fin = 0;
+    SJF[2].wait = 0;
+    SJF[2].tat = 0;
+    //--------------------
+    SJF[3].ch = 'D';
+    SJF[3].arv = 4;
+    SJF[3].run = 3;
+    SJF[3].rem = SJF[3].run;
+    SJF[3].exe = -1;
+    SJF[3].fin = 0;
+    SJF[3].wait = 0;
+    SJF[3].tat = 0;
+*/
     //print
-    cout<<"\tArv\tRun\tWait "<<endl;
+    cout<<"\tArv\tRun "<<endl;
     for(int i=0;i<n;i++)
     {
-        cout<<SJF[i].ch<<"\t"<<SJF[i].arv<<"\t"<<SJF[i].run<<"\t"<<SJF[i].wait<<endl;
+        cout<<SJF[i].ch<<"\t"<<SJF[i].arv<<"\t"<<SJF[i].run<<endl;
     }
-    cout<<index<<"  --  now : "<<Now<<endl<<endl;
-
-
-
-
-    SRTF();
-    SJF_();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*for(int i=0;i<n;i++)
+    cout<<"Time now : "<<Now<<endl<<endl;
+    int flag=0;
+    while(flag !=3)
     {
-        SJF[i].rem = SJF[i].run ;
-        SJF[i].exe = -1;
-        SJF[i].fin = 0;
-        SJF[i].wait = 0;
-        SJF[i].tat = 0;
+        cout<<"if you want  SJF non_preemptive press 1   /   SJF preemptive press 2  : ";
+        cin>>flag;
+        cout<<endl;
+        if (flag == 1)
+            SJF_();
+        else if(flag == 2)
+            SRTF();
+        for(int i=0;i<n;i++)
+        {
+            SJF[i].rem = SJF[i].run ;
+            SJF[i].exe = -1;
+            SJF[i].fin = 0;
+            SJF[i].wait = 0;
+            SJF[i].tat = 0;
+        }
     }
-    //print
-    cout<<"\tArv\tRun\tRem\texe\tFin\tTAT\tWait "<<endl;
-    for(int i=0;i<n;i++)
-    {
-        cout<<SJF[i].ch<<"\t"<<SJF[i].arv<<"\t"<<SJF[i].run<<"\t"<<SJF[i].rem<<"\t"<<SJF[i].exe<<"\t"<<SJF[i].fin<<"\t"<<SJF[i].tat<<"\t"<<SJF[i].wait<<endl;
-    }
-    */
-
-
     return 0;
 }
